d4006ce0c9fa117f4da9bd0e3124fed46da5690c
chudinhhieu <109270474+chudinhhieu@users.noreply.github.com>
2023-03-17 07:41:01 +0700

    Chu Hieu them thu muc

---
+++ app/src/main/java/hieucdph29636/fpoly/tuhubread/Database/DBHelper.java
@@ -0,0 +1,23 @@
+package hieucdph29636.fpoly.tuhubread.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context, "TuHuBread", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
