c11b4ae3c6e79b3dd74351a1691e559f8ace1a61
chudinhhieu <109270474+chudinhhieu@users.noreply.github.com>
2023-03-30 12:30:01 +0700

    Update DBHelper.java

--- app/src/main/java/hieucdph29636/fpoly/tuhubread/DBHelper/DBHelper.java
+++ app/src/main/java/hieucdph29636/fpoly/tuhubread/DBHelper/DBHelper.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
     public static final String DB_NAME="Duan1";
-    public static final int DB_VERSION=1;
+    public static final int DB_VERSION=100;
     public DbHelper(@Nullable Context context) {
         super(context, DB_NAME, null,DB_VERSION);
     }
@@ -17,18 +17,43 @@ public class DbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String sql_Loai="create table LoaiMonAn("+"id_loaiDoAn integer primary key Autoincrement,"+"tenLoai text not null)";
         db.execSQL(sql_Loai);
+        db.execSQL("INSERT INTO LoaiMonAn(id_loaiDoAn,tenLoai) VALUES(0,'Bánh mì'),(1,'Sandwich'),(2,'Nước')");
         String sql_Mon="create table MonAn("+"id_MonAn integer primary key autoincrement,"+
-                "tenMon text not null,"+"gia number not null,"+
-                "moTa text not null,"+"thanhPhan text not null,"+
-                "trangThai text not null,"+"id_loaiDoAn integer references MonAn(id_MonAn),"+
-                "anhMonAn blod not null)";
+                "tenMon text not null,"+"gia number not null,"+ "thanhPhan text,"+
+                "trangThai integer not null,"+"id_loaiDoAn integer references LoaiMonAn(id_loaiDoAn),"+
+                "anhMonAn blod)";
         db.execSQL(sql_Mon);
+        db.execSQL("INSERT INTO MonAn (id_MonAn, tenMon, gia,thanhPhan,trangThai,id_loaiDoAn,anhMonAn)\n" +
+                "VALUES (1,'Bánh mì xúc xích bò băm hầm',30000,'Thành phần: Xúc xích bò băm hầm,rau mùi,hành tây',1,0,null)," +
+                "(2,'Bánh mì xá xíu hongkong',35000,'Thành phần: Thịt xá xíu HongKong,rau mùi,dưa chuột,rau mix,sốt xá xíu,sốt hồng Tuhu',1,0,null)," +
+                "(3,'Bánh mì thịt heo nướng phá lấu',35000,'Thành phần: Thịt heo phá lấu,Pate,rau mùi,dưa chuột,rau mix,sốt hồng Tuhu',1,0,null)," +
+                "(4,'Bánh mì gà xoài cay',35000,'Thành phần: Gà cay,rau mix,xoài,sốt hồng Tuhu',1,0,null)," +
+                "(5,'Bánh mì gà nướng sả',30000,'Thành phần: Gà nướng sả,Pate,rau mùi,dưa chuột,rau mix,sốt hồng Tuhu',1,0,null)," +
+                "(6,'Bánh mì gà Hawaii',35000,'Thành phần: Gà Hawaii,rau mùi,hành tây tím,bắp cải tím,dứa,sốt hồng Tuhu',1,0,null)," +
+                "(7,'Bánh mì bò sốt tiêu đen',45000,'Thành phần: Bò sốt tiêu đen,dưa chuột,rau mùi,hành tây,bắp cải tím,sốt hồng Tuhu',1,0,null)," +
+                "(8,'Bánh mì bò sốt phô mai trứng muối',50000,'Thành phần: Bò sốt phô mai trứng muối,dưa chuột,rau mùi,hành tím muối,rau mix,sốt hồng Tuhu',1,0,null)," +
+                "(9,'Bánh mì bò miếng sốt phô mai',45000,'Thành phần: Bò sốt phô mai,dưa chuột,rau mùi,hành tím muối,rau mix,sốt hồng Tuhu',1,0,null)," +
+                "(10,'Sandwich phô mai sữa',30000,'Thành phần: Sandwich nóng giòn kèm theo phô mai sữa',1,1,null)," +
+                "(11,'Sandwich phô mai Socola',30000,'Thành phần: Sandwich nóng giòn kèm theo phô mai socola thanh ngọt',1,1,null)," +
+                "(12,'Sandwich phô mai xúc xích',30000,'Thành phần: Sandwich nóng giòn kèm theo phô mai xúc xích tiệt trùng',1,1,null)," +
+                "(13,'Sandwich phô mai pate',30000,'Thành phần: Sandwich nóng giòn kèm theo phô mai pate chất lượng',1,1,null)," +
+                "(14,'Sandwich phô mai jambon',30000,'Thành phần: Sandwich nóng giòn kèm theo phô mai jambon heo',1,1,null)," +
+                "(15,'Trà tắc khổng lồ',15000,'Thành phần: Trà tắc mát lạnh cùng với size cực đại',1,2,null)," +
+                "(16,'Trà cozy sữa',25000,'Thành phần: Trà Cozy mát lạnh cùng với vị sữa thanh mát',1,2,null)," +
+                "(17,'Trà chanh khổng lồ',15000,'Thành phần: Trà chanh mát lạnh cùng với size cực đại',1,2,null)," +
+                "(18,'Soda mojito',25000,'Thành phần: Soda mới lạ vị chanh',1,2,null)," +
+                "(19,'Milo cafe',25000,'Sữa Milo kết hợp cùng vị cà phê mới lạ',1,2,null);");
         String sql_KhuyenMai="create table KhuyenMai("+"id_KhuyenMai integer primary key autoincrement,"+"code text not null,"+
                 "moTaKM text not null,"+
                 "ngayBatDau date not null,"+
                 "ngayKetThuc date not null,"+
                 "soTienGiam number not null)";
         db.execSQL(sql_KhuyenMai);
+        db.execSQL("INSERT INTO KhuyenMai(id_KhuyenMai,code,moTaKM,ngayBatDau,ngayKetThuc,soTienGiam) VALUES(1,'code1','Khuyến mãi 1','11/02/2023','15/02/2023',15000)," +
+                "(2,'code2','Khuyến mãi 2','11/02/2023','15/02/2023',15000)," +
+                "(3,'code3','Khuyến mãi 3','11/02/2023','15/02/2023',15000)," +
+                "(4,'code4','Khuyến mãi 4','11/02/2023','15/02/2023',15000)," +
+                "(5,'code5','Khuyến mãi 5','11/02/2023','15/02/2023',15000)");
         //Khách hàng
         String sql_KhachHang = "create table KhachHang(" + "id_makhachhang integer primary key Autoincrement," +
                 "hoTen text not null," + "soDienThoai text not null," +
@@ -48,22 +73,26 @@ public class DbHelper extends SQLiteOpenHelper {
         db.execSQL(sql_DonHang);
         //Chi tiết đơn hàng
         String sql_ChiTietDonHang = "create table ChiTietDonHang(" + "id_ct integer primary key Autoincrement," +
-                "id_donHang integer references DonHang," + "id_monAn integer references MonAn," +
+                "id_donHang integer references DonHang(id_madonhang)," + "id_monAn integer references MonAn(id_MonAn)," +
                 "soLuong int, " + "giaTien number not null)";
         db.execSQL(sql_ChiTietDonHang);
         //Món ăn yêu thích
-        String sql_MonAnYeuThich = "create table MonAnYeuThich(" + "id_mamonan integer references MonAn," + "id_khachHang integer references KhachHang)";
+        String sql_MonAnYeuThich = "create table MonAnYeuThich(" + "id_mamonan integer references MonAn(id_MonAn)," + "id_khachHang integer references KhachHang(id_makhachhang))";
         db.execSQL(sql_MonAnYeuThich);
-
         // Nhân Viên
-        String sql_NhanVien="create table NhanVien("+"id_NhanVien integer primary key autoincrement,"+"code text not null,"+
+        String sql_NhanVien="create table NhanVien("+"id_NhanVien integer primary key autoincrement,"+
                 "hoTen text not null,"+
                 "soDienThoai text not null,"+
                 "taiKhoan text not null,"+
                 "matKhau text not null,"+
                 "ngaySinh date not null,"+
-                "quyenNhanVien text not null)";
+                "quyenNhanVien integer not null)";
         db.execSQL(sql_NhanVien);
+        db.execSQL("insert into NhanVien(id_NhanVien,hoTen,soDienThoai,taiKhoan,matKhau,ngaySinh,quyenNhanVien)values(1,'Nguyễn Văn An',0912345644,'nhanvien1','123456','20/11/2020',1)");
+        db.execSQL("insert into NhanVien(id_NhanVien,hoTen,soDienThoai,taiKhoan,matKhau,ngaySinh,quyenNhanVien)values(2,'Nguyễn Thị Bình',0912345644,'nhanvien2','123456','20/11/2020',0)");
+        db.execSQL("insert into NhanVien(id_NhanVien,hoTen,soDienThoai,taiKhoan,matKhau,ngaySinh,quyenNhanVien)values(3,'Trần Văn Chinh',0912345644,'nhanvien3','123456','20/11/2020',2)");
+        db.execSQL("insert into NhanVien(id_NhanVien,hoTen,soDienThoai,taiKhoan,matKhau,ngaySinh,quyenNhanVien)values(4,'Đỗ Khánh Đoàn ',0912345644,'nhanvien4','123456','20/11/2020',2)");
+
 
         // Đánh giá
         String sql_DanhGia="create table DanhGia("+"id_DanhGia integer primary key autoincrement,"+"code text not null,"+
@@ -75,13 +104,18 @@ public class DbHelper extends SQLiteOpenHelper {
         db.execSQL(sql_DanhGia);
 
         // Đơn nạp tiền
-        String sql_DonNapTien="create table DonNapTien("+"id_DonNapTien integer primary key autoincrement,"+"code text not null,"+
+        String sql_DonNapTien="create table DonNapTien("+"id_DonNapTien integer primary key autoincrement,"+
                 "id_khachHang integer references KhachHang(id_makhachhang),"+
                 "thoiGianTao date not null,"+
                 "trangThai integer not null,"+
                 "tienNap number not null,"+
-                "anhHoaDon blob not null)";
+                "anhHoaDon blob )";
         db.execSQL(sql_DonNapTien);
+        db.execSQL("insert into DonNapTien(id_DonNapTien,id_khachHang,thoiGianTao,trangThai,tienNap,anhHoaDon)values(1,2,'25/3/2023',1,35000,null)");
+        db.execSQL("insert into DonNapTien(id_DonNapTien,id_khachHang,thoiGianTao,trangThai,tienNap,anhHoaDon)values(2,3,'20/3/2023',0,30000,null)");
+        db.execSQL("insert into DonNapTien(id_DonNapTien,id_khachHang,thoiGianTao,trangThai,tienNap,anhHoaDon)values(3,1,'15/3/2023',1,70000,null)");
+        db.execSQL("insert into DonNapTien(id_DonNapTien,id_khachHang,thoiGianTao,trangThai,tienNap,anhHoaDon)values(4,3,'20/3/2023',0,15000,null)");
+
 
 
     }
